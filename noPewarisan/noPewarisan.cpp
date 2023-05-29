8e7d3081e8312522d47286c36a64e8a3fb3e49cd
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-05-29 20:25:44 +0700

    Kelas baseClass dideklarasikan sebagai kelas terakhir (final). Ini berarti bahwa kelas baseClass tidak dapat diturunkan oleh kelas lain. Kelas ini memiliki sebuah fungsi perkenalan() yang dideklarasikan sebagai virtual. Dalam implementasinya, fungsi ini mencetak pesan "Hallo saya function dari base class".

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -7,17 +7,3 @@ public:
 		cout << "Hallo saya function dari base class";
 	}
 };
-
-class derivedClass : public baseClass {
-public:
-	void perkenalan() {
-		cout << "Hallo saya Function dari derived Class";
-	}
-};
-
-int main() {
-	derivedClass a;
-	a.perkenalan();
-
-	return 0;
-}
