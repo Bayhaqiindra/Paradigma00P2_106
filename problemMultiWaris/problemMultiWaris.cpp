a9455fad7f8dc9f9c9f88cb456f2b99b06af5b65
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-05-29 19:57:44 +0700

    sebuah kelas bernama "orang" yang memiliki satu variabel anggota publik yaitu "umur" (age). Kelas ini juga memiliki sebuah konstruktor yang mengambil parameter integer "pUmur" (pAge) dan menginisialisasi variabel anggota "umur" dengan nilai yang diteruskan ke konstruktor.

--- problemMultiWaris/problemMultiWaris.cpp
+++ problemMultiWaris/problemMultiWaris.cpp
@@ -1,10 +1,51 @@
-// problemMultiWaris.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+class orang {
+	public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+		{
+			cout << "orang dibuat dengan umut " << umur << "\n" << endl;
+	}
+};
+
+class pekerja : public orang {
+	public:
+
+	pekerja(int pUmur) :
+		orang(pUmur)
+		{
+			cout << "pekerja dibuatin" << endl;
+	}
+};
+
+class pelajar :public orang {
+	public:
+
+	pelajar(int pUmur) :
+		orang(pUmur)
+		{
+			cout << "pelajar dibuat\n" << endl;
+	}
+};
+
+class budi :public pekerja, public pelajar {
+	public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+		{
+			cout << "Budi dibuat\n" << endl;
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	budi a(12);
 
+	return 0;
+}
